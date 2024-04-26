f55a57129807e6eae9bb491e5f20b5dfcb12fa93
SSFSKIM <kimmi@ssfs.org>
2024-04-26 14:09:16 -0400

    Initial Commit

---
+++ meal tracker/Food.swift
@@ -0,0 +1,13 @@
+//
+//  Food.swift
+//  meal tracker
+//
+//  Created by Steve on 4/26/24.
+//
+
+import Foundation
+
+struct Food {
+    var name: String
+    var description: String
+}
---
+++ meal tracker/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  meal tracker
+//
+//  Created by Steve on 4/26/24.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String
+    var food: [Food]
+}
